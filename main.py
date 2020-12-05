0ee116ae2fdbe897f36e75c7fd20c0dcbe0dc0dd
Lukas Vozenilek <lvozenilek@gmail.com>
2020-12-04 17:49:58 -0800

    Tuning parameters, cleaning up code

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import chess, random, time, math, chess.pgn, chess.engine, datetime
 firstmovesdict = {}
 
 # Settings
-maxdepth = 1
 random_mag = 0
 centerbonus = 0.25
 centerattackbonus = 0.25
-checkbonus = 1
+checkbonus = 0.5
 endbonus = 15
 castlebonus = 0.5
 
@@ -33,8 +32,8 @@ devbonus = {chess.PAWN: 0.5,
             chess.KNIGHT: 0.25,
             chess.BISHOP: 0.25,
             chess.ROOK: -0.25,
-            chess.QUEEN: -0.25,
-            chess.KING: -1
+            chess.QUEEN: -1,
+            chess.KING: -2
             }
 
 commands = {"a": "Analyze", "p": "Play", "h": "Help", "q": "Quit"}
@@ -54,14 +53,11 @@ def analyzemove(board, move, alpha, beta, maximize, depthleft):
         score = evaluatemove(board, move)
         if maximize:
             score = -score
-        #print("Last depth, score " + str(score))
         return (move, score)
-
 
     pushed = False
     if move is not None:
         thismovescore = evaluatemove(board, move)
-        #print("This moves score is " + str(thismovescore))
         board.push(move)
         pushed = True
     else:
@@ -74,7 +70,6 @@ def analyzemove(board, move, alpha, beta, maximize, depthleft):
     if maximize:
         maxScore = -10000
         for move in presort(board, board.legal_moves):
-            #print("Maxing move " + str(move))
             score = analyzemove(board, move, alpha, beta, False, depthleft-1)[1]
             if (thismovescore is not None):
                 score -= thismovescore
@@ -83,118 +78,26 @@ def analyzemove(board, move, alpha, beta, maximize, depthleft):
             maxScore = max(score, maxScore)
             alpha = max(alpha, score)
             if beta <= alpha:
-                #print("Beta hard cutoff")
                 break
         if pushed:
             board.pop()
-        #print(maxScore)
-        #print("LEAVING DEPTH, RESULTS:\n")
-        #print("Best move: " + str(maxScore) + ":" + str(bestmove))
-        #print("Alpha: " + str(alpha))
-        #print("Beta: " + str(beta))
         return (bestmove, maxScore)
     else:
         minScore = 10000
         for move in presort(board, board.legal_moves):
-            #print("Minning move " + str(move))
             score = analyzemove(board, move, alpha, beta, True, depthleft-1)[1]
             if (thismovescore is not None):
                 score += thismovescore
-            #print(score)
-            #print(minScore)
             if score < minScore:
                 worstMove = move
             minScore = min(score, minScore)
             beta = min(beta, score)
-            #print(alpha)
-            #print(beta)
             if beta <= alpha:
-                #print("Alpha hard cutoff")
                 break
         if pushed:
             board.pop()
-        #print("LEAVING DEPTH, RESULTS:\n")
-        #print("Worst move: " + str(worstMove) + ":" + str(minScore))
-        #print("Alpha: " + str(alpha))
-        #print("Beta: " + str(beta))
         return (worstMove, minScore)
 
-
-'''
-
-def analyzemove(board, move, alpha, beta, max, depthleft):
-    # If on last depth return just the evaluation of the given move
-    if depthleft == 0:
-        score = evaluatemove(board, move)
-        print("Last depth, score " + str(score))
-        return score
-
-    print("##############ENTERING NEXT DEPTH###########")
-    color = board.turn
-    all_moves = {}
-
-    pushedmove = False
-    # If a move was passed to us, push it on the board
-    if move is not None:
-        board.push(move)
-        pushedmove = True
-    
-    
-    for move in presort(board, board.legal_moves):
-        print("Evaluating move " + str(move))
-        movescore = analyzemove(board, move, alpha, beta, not max, depthleft - 1)
-        if max:
-            if (movescore >= beta):
-                print("Beta hard cutoff")
-                return beta  # fail hard beta-cutoff
-            if (movescore > alpha):
-                alpha = movescore  # alpha acts like max in MiniMax
-                print("Setting new alpha of " + str(alpha))
-            #return alpha
-        else:
-            if (movescore <= alpha):
-                print("Alpha hard cutoff")
-                return alpha  # fail hard alpha cutoff
-            if (movescore < beta):
-                beta = movescore  # beta acts like min in MiniMax
-                print("Setting new beta of " + str(beta))
-            #return beta
-        #all_moves[move] = finalvalue
-
-    if pushedmove:
-        board.pop()
-
-    print("##############LEAVING DEPTH###########")
-    if max:
-        return alpha
-    else:
-        return beta
-
-beta is the minimum score the maximizing play is assured of
-alpha is the maximum score the minimizing player is assured of
-
-def alphaBetaMax(alpha, beta, depthleft):
-    if (depthleft == 0): return evaluate()
-    for (all moves):
-        score = alphaBetaMin(alpha, beta, depthleft - 1)
-        if (score >= beta):
-            return beta  # fail hard beta-cutoff
-        if (score > alpha):
-            alpha = score  # alpha acts like max in MiniMax
-    return alpha
-
-
-def alphaBetaMin(alpha, beta, depthleft)
-    if (depthleft == 0): return -evaluate()
-    for (all moves):
-        score = alphaBetaMax(alpha, beta, depthleft - 1)
-        if (score <= alpha):
-            return alpha  # fail hard alpha-cutoff
-        if (score < beta):
-            beta = score  # beta acts like min in MiniMax
-    return beta
-
-'''
 
 
 # Contains all evaluation logic for a given move on a board
@@ -212,9 +115,6 @@ def evaluatemove(board, move):
             piece = chess.PAWN
         else:
             piece = board.piece_at(move.to_square).piece_type
-        if verbose:
-            pass
-            # log("Capture of value: " + str(pointsdict[piece]))
         finalvalue += pointsdict[piece]
 
     earlygain = (earlycurve_k * math.pow(earlycurve_base, -movenum + earlycurve_o))
@@ -228,16 +128,10 @@ def evaluatemove(board, move):
         if (movepiece.piece_type == chess.PAWN and (move.from_square in chess.SquareSet(chess.BB_RANK_7))) or (
                 move.from_square in chess.SquareSet(chess.BB_RANK_8)):
             devbonusi = earlygain * devbonus[movepiece.piece_type]
-            if verbose:
-                pass
-                # log("Applying development bonus of " + str(devbonusi))
             finalvalue += devbonusi
 
     # Center position
     if move.to_square in chess.SquareSet(chess.BB_CENTER):
-        if verbose:
-            pass
-            # log("Applying center position bonus")
         finalvalue += earlygain * centerbonus
 
     # Check
@@ -251,9 +145,6 @@ def evaluatemove(board, move):
     board.push(move)
 
     # Center attack
-    if verbose & (len(chess.SquareSet(chess.BB_CENTER) & board.attacks(move.to_square)) > 0):
-        pass
-        # log("Applying center attack bonus of: " + str(earlygain * centerattackbonus * len(chess.SquareSet(chess.BB_CENTER) & aboard.attacks(move.to_square))))
     finalvalue += earlygain * centerattackbonus * len(
         chess.SquareSet(chess.BB_CENTER) & board.attacks(move.to_square))
 
@@ -275,14 +166,8 @@ def evaluatemove(board, move):
     if movepiece.piece_type == chess.PAWN:
         currank = chess.square_rank(move.from_square)
         if color == chess.WHITE and move.to_square in chess.SquareSet(chess.BB_RANK_8):
-            if (verbose):
-                pass
-                # log("Applying promotion bonus")
             finalvalue += 8
         if color == chess.BLACK and move.to_square in chess.SquareSet(chess.BB_RANK_1):
-            if (verbose):
-                pass
-                # log("Applying promotion bonus")
             finalvalue += 8
 
     return finalvalue
@@ -301,17 +186,17 @@ def Human(board):
     return move
 
 
-def LukasEngine(board):
+def LukasEngine(board, depth):
     global moves_checked
     starttime = time.time()
-    move = analyzemove(board, None, -10, 10, True, 4)[0]
+    move = analyzemove(board, None, -10, 10, True, int(depth))[0]
     # log()(f'{moves_checked:,}' + " possible moves analysed!")
     # log()(f'{moves_checked / (time.time() - starttime):,}' " moves/second")
     moves_checked = 0
     return move
 
 
-def PlayGame(player1, p1name, player2, p2name):
+def PlayGame(player1, p1name, p1depth, player2, p2name, p2depth):
     pgnfile = open("lastgame.pgn", "w")
     board = chess.Board()
     starttime = time.time()
@@ -325,7 +210,7 @@ def PlayGame(player1, p1name, player2, p2name):
     while not board.is_game_over(claim_draw=True):
         if board.is_game_over():
             break
-        move = player1(board)
+        move = player1(board, p1depth)
         if move == "/e":
             break
         node = node.add_variation(move)
@@ -335,7 +220,7 @@ def PlayGame(player1, p1name, player2, p2name):
 
         if board.is_game_over():
             break
-        move = player2(board)
+        move = player2(board, p2depth)
         if move == "/e":
             break
         node = node.add_variation(move)
@@ -352,20 +237,18 @@ def PlayGame(player1, p1name, player2, p2name):
 
 
 
-def AnalyzeFen(fen):
+def AnalyzeFen(fen, depth):
     global verbose
     verbose = True
     board = chess.Board(fen)
-
-    results = analyzemove(board, None, -10000, 10000, True, 4)
+    results = analyzemove(board, None, -10000, 10000, True, int(depth))
     print("\n\n")
     print(results)
     verbose = False
 
 
-
-def StockFish(board):
-    move = engine.play(board, chess.engine.Limit(time=0.05))
+def StockFish(board, depth):
+    move = engine.play(board, chess.engine.Limit(time=0.01))
     return move.move
 
 
@@ -376,10 +259,6 @@ def ParsePlayer(string):
         return (StockFish, "StockFish")
     if string == "l":
         return (LukasEngine, "LukasEngine")
-
-
-#AnalyzeFen("rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2")
-#AnalyzeFen("rnb1kbnr/ppp1pppp/8/3q4/8/8/PPPP1PPP/RNBQKBNR w KQkq - 0 3")
 
 
 if __name__ == "__main__":
@@ -409,13 +288,17 @@ if __name__ == "__main__":
             print("Analysis complete.")
 
         elif cmd == "p":
+            depth1 = 0
+            depth2 = 0
             p1 = input("White player (h, l or s): ")
+            if (p1 == "l"):
+                depth1 = input("Enter Depth for p1: ")
             p2 = input("Black player (h, l or s): ")
-            if (p1 == "l") or (p2 == "l"):
-                depth = input("Enter Depth: ")
+            if (p2 == "l"):
+                depth2 = input("Enter Depth for p2: ")
 
             print("Starting game!")
-            PlayGame(ParsePlayer(p1)[0], ParsePlayer(p1)[1], ParsePlayer(p2)[0], ParsePlayer(p2)[1])
+            PlayGame(ParsePlayer(p1)[0], ParsePlayer(p1)[1], depth1, ParsePlayer(p2)[0], ParsePlayer(p2)[1], depth2)
 
         elif cmd == "h":
             for item in commands:
